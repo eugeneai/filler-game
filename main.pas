bea27d03c1bcb87a2844f367d1020e70cb7824f6
Evgeny Cherkashin <eugeneai@irnok.net>
2015-11-26 18:35:20 +0800

    Added field painting sd human choice.

--- main.pas
+++ main.pas
@@ -13,6 +13,9 @@ const
   BOARD_Y=30;
   MAX_HALF_STEPS=4;
   MAX_COLOR=5; { 1..5 }
+  COLORS: array [1..MAX_COLOR] of TColor =(
+  clYellow, clGreen, clWhite, clBlack, clPurple
+  );
 type
   TD = array [1..4] of integer;
 
@@ -25,18 +28,32 @@ type
   { TFillerForm }
 
   TFillerForm = class(TForm)
+    human: TLabel;
+    computer: TLabel;
     NewGame: TButton;
+    Choice: TPaintBox;
+    human_col: TPaintBox;
+    computer_col: TPaintBox;
     PlayBoard: TPaintBox;
+    procedure ChoiceMouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure FormCreate(Sender: TObject);
+    procedure humanClick(Sender: TObject);
+    procedure human_colPaint(Sender: TObject);
     procedure NewGameClick(Sender: TObject);
     function MiniMax(hum:boolean; d:integer; board:TBoard; var best_board: TBoard):integer;
     function Account(board:TBoard):integer;
     procedure MakeStep(hum:boolean; board:TBoard; var new_board:TBoard; var n:byte);
     procedure FillBoard(var board:TBoard; n,pn:byte; x, y:integer);
+    procedure ChoiceClick(Sender: TObject);
     procedure PlayBoardPaint(Sender: TObject);
+    procedure ChoicePaint(Sender: TObject);
   private
     { private declarations }
+    procedure BoardCreate;
   public
     { public declarations }
+    board:TBoard;
   end;
 
 var
@@ -50,7 +67,85 @@ implementation
 
 procedure TFillerForm.NewGameClick(Sender: TObject);
 begin
+  BoardCreate;
+  human_col.Repaint;
+  computer_col.Repaint;
+  PlayBoard.Repaint;
+end;
 
+procedure TFillerForm.BoardCreate;
+var
+  x,y:integer;
+begin
+  board:=TBoard.Create;
+  Randomize;
+  for x:=1 to BOARD_X do
+  begin
+    for y:=1 to BOARD_Y do
+    begin
+      board.b[x,y]:=Random(MAX_COLOR)+1;
+    end;
+  end;
+  board.c:=board.b[1,BOARD_Y];
+  board.h:=board.b[BOARD_X,1];
+  if board.c=board.h then
+  begin
+    board.c:=((board.c) mod MAX_COLOR)+1;
+    board.b[1,BOARD_Y]:=board.c;
+  end;
+  board.correct:=true;
+end;
+
+procedure TFillerForm.FormCreate(Sender: TObject);
+begin
+  BoardCreate;
+end;
+
+procedure TFillerForm.ChoiceMouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+var
+  nh:Byte;
+  W:Integer;
+  s:Integer;
+begin
+  W:=Choice.ClientWidth;
+  s:=W div MAX_COLOR;
+  nh:=(X div s)+1;
+  if board.h=nh then
+  begin
+    ShowMessage('You cannot take this color as it already takey by You!');
+    exit;
+  end;
+  if board.c=nh then
+  begin
+    ShowMessage('You cannot take this color as it already taken by computer!');
+    exit;
+  end;
+  board.h:=nh;
+  human_col.Repaint;
+end;
+
+procedure TFillerForm.humanClick(Sender: TObject);
+begin
+
+end;
+
+procedure TFillerForm.human_colPaint(Sender: TObject);
+var
+  C:TCanvas;
+  W,H:Integer;
+begin
+  C:=human_col.Canvas;
+  W:=human_col.ClientWidth;
+  H:=human_col.ClientHeight;
+  C.Brush.Color:=COLORS[board.h];
+  C.Rectangle(0,0, W,H);
+
+  C:=computer_col.Canvas;
+  W:=computer_col.ClientWidth;
+  H:=computer_col.ClientHeight;
+  C.Brush.Color:=COLORS[board.c];
+  C.Rectangle(0,0, W,H);
 end;
 
 function TFillerForm.MiniMax(hum:boolean; d:integer; board:TBoard;
@@ -163,16 +258,58 @@ begin
   end;
 end;
 
-procedure TFillerForm.PlayBoardPaint(Sender: TObject);
+procedure TFillerForm.ChoiceClick(Sender: TObject);
 var
-  C:TCanvas;
+  m:TPoint;
 begin
-  C:=PlayBoard.Canvas;
-  //Cx:=Figure.ClientWidth >> 1;
-  //Cy:=Figure.ClientHeight >> 1;
-  //C.Draw(Cx-Sx,by,bmp);  //TCanvas
-  //C.Ellipse(xi-SR,yi-SR,xi+SR,yi+SR);
+  m:=Mouse.CursorPos;
+
 end;
 
+procedure TFillerForm.PlayBoardPaint(Sender: TObject);
+var
+  i, r, f, x,y: integer;
+  C:TCanvas;
+  W,H:Integer;
+  sx,sy:Integer;
+begin
+  C:=PlayBoard.Canvas;
+  C.Brush.Color:= clRed;
+  C.Pen.Color := clBlue;
+  W:=PlayBoard.ClientWidth;
+  H:=PlayBoard.ClientHeight;
+  sx:=W div BOARD_X;
+  sy:=H div BOARD_Y;
+  //C.Rectangle(0,0,C.Width, C.Height);
+  for x:=0 to BOARD_X-1 do
+  begin
+    for y:=0 to BOARD_Y-1 do
+    begin
+      C.Brush.Color:=COLORS[board.b[x+1,y+1]];
+      C.Rectangle(x*sx,y*sy,(x+1)*sx,(y+1)*sy);
+    end;
+  end;
+end;
+
+
+procedure TFillerForm.ChoicePaint(Sender: TObject);
+var
+  i, r, f, x,y: integer;
+  C:TCanvas;
+  W,H:Integer;
+  sx,sy:Integer;
+begin
+  C:=Choice.Canvas;
+  C.Brush.Color:= clRed;
+  C.Pen.Color := clBlue;
+  W:=Choice.ClientWidth;
+  H:=Choice.ClientHeight;
+  sx:=W div MAX_COLOR;
+  for x:=0 to MAX_COLOR-1 do
+  begin
+    C.Brush.Color:=COLORS[x+1];
+    C.Rectangle(x*sx,0,(x+1)*sx,H);
+  end;
+end;
 end.
 
